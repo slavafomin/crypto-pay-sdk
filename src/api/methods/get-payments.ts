ba100b2504ba0924f10c8d105ae62858ae0e28c6
Slava Fomin II <slava@fomin.io>
2021-11-29 12:48:59 +0300

    Implemented `getPayments()` method
    Signed-off-by: Slava Fomin II <slava@fomin.io>

--- src/api/methods/get-payments.ts
+++ src/api/methods/get-payments.ts
@@ -1,63 +1,88 @@
 
+import Joi from 'joi';
+
+import { omitEmptyProps } from '../../common/utils';
+import { HttpClient, HttpRequestMethod } from '../../http-client/http-client';
+import { Invoice, InvoiceResponse, parseInvoiceResponse } from '../common/invoice';
+import { HttpApiResponse, makeRequest } from '../common/make-request';
+import { Network } from '../common/network';
+import { PaginationParams, paginationSerializers, paginationValidators } from '../common/pagination';
+import { transformResponse } from '../common/transform-response';
+import { AppToken } from '../common/types';
+
+
 export interface GetPaymentsRequestOptions {
+  appToken: AppToken;
   params: GetPaymentsParams;
+  httpClient: HttpClient;
+  network?: Network;
 }
 
-export interface GetPaymentsParams {
-
-  /**
-   * Optional. Offset needed to return a specific
-   * subset of invoices. Default 0.
-   */
-  offset?: number;
-
-  /**
-   * Optional. Number of invoices to return.
-   * Default 100, max 1000.
-   */
-  count?: number;
-
+export interface GetPaymentsParams extends PaginationParams {
 }
 
-export interface GetPaymentsRequest {
-
-  /**
-   * Optional. Offset needed to return a specific
-   * subset of invoices. Default 0.
-   */
-  offset?: number;
-
-  /**
-   * Optional. Number of invoices to return.
-   * Default 100, max 1000.
-   */
-  count?: number;
-
+export interface GetPaymentsRequest extends PaginationParams {
 }
 
 export interface GetPaymentsResponse {
-  // @todo
+  count: number;
+  items: InvoiceResponse[];
+}
+
+export interface GetPaymentsResult {
+  count: number;
+  items: Invoice[];
 }
 
 
 export async function getPayments(
   options: GetPaymentsRequestOptions
 
-): Promise<GetPaymentsResponse> {
+): Promise<HttpApiResponse<GetPaymentsResult>> {
 
-  const { params } = options;
+  const {
+    appToken,
+    httpClient,
+    network,
 
-  // @todo: set defaults
-  // @todo: validate params
+  } = options;
+
+  const paramsSchema = Joi.object({
+    ...paginationValidators,
+  });
+
+  const validationResult = await paramsSchema
+    .validateAsync(options.params || {}, {
+      warnings: true,
+    })
+  ;
+
+  const params: GetPaymentsParams = (
+    validationResult.value
+  );
 
   // Request serialization
-  const payload: GetPaymentsRequest = {
-    offset: params.offset,
-    count: params.count,
-  };
+  const query = omitEmptyProps<GetPaymentsRequest>({
+    ...paginationSerializers(params),
+  });
 
-  // @todo: make request
+  const response = await makeRequest<
+    GetPaymentsRequest,
+    GetPaymentsResponse
+  >({
+    appToken,
+    httpClient,
+    methodName: 'getPayments',
+    query,
+    httpMethod: HttpRequestMethod.Get,
+    network,
+  });
 
-  return {};
+  return transformResponse(response, result => ({
+    count: result.count,
+    items: result.items.map(
+      item => parseInvoiceResponse(item)
+    )
+  }));
 
 }
